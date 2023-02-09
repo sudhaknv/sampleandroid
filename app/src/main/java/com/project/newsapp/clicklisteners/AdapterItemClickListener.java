056bea83f2fd3ff176ba2893f311f1b9093c95b0
sudhaknv <49087665+sudhaknv@users.noreply.github.com>
2023-02-09 14:24:44 +0530

    Delete app directory

--- app/src/main/java/com/project/newsapp/clicklisteners/AdapterItemClickListener.java
+++
@@ -1,9 +0,0 @@
-package com.project.newsapp.clicklisteners;
-
-import com.project.newsapp.model.News;
-
-public interface AdapterItemClickListener {
-
-    void onNewsItemClick(News news);
-
-}
--- app/src/main/java/com/project/newsapp/clicklisteners/NewsDialogClickListeners.java
+++
@@ -1,8 +0,0 @@
-package com.project.newsapp.clicklisteners;
-
-public interface NewsDialogClickListeners {
-
-    void onGotoWebSiteClick(String url);
-    void onDismissClick();
-
-}
